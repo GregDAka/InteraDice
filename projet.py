31c480e029ab7380fab549cecd1eec29ebd27b89
Tom <102308680+TfAplo@users.noreply.github.com>
2025-04-06 16:20:14 +0200

    version finale

--- projet.py
+++ projet.py
@@ -1,49 +1,137 @@
 from ursina import *
 import random
 import trimesh
+import numpy as np
 from ursina.shaders import lit_with_shadows_shader
 from ursina.prefabs.dropdown_menu import DropdownMenu, DropdownMenuButton
 from ursina.prefabs.radial_menu import RadialMenu, RadialMenuButton
 
 app = Ursina()
 
-# Création d'un icosaèdre avec trimesh
-icosahedron_mesh = trimesh.creation.icosahedron()
+sky = Sky()
+
+icosahedron_mesh = trimesh.creation.box(extents=[2, 2, 2])
 
 vertices = icosahedron_mesh.vertices.tolist()
-faces = icosahedron_mesh.faces.tolist()
+faces = [face[::-1] for face in icosahedron_mesh.faces.tolist()]
 normals = icosahedron_mesh.vertex_normals.tolist() #les normals correspondent à l'orientation des faces
-
-# Génération de coordonnées UV simples (pour pouvoir appliquer les textures)
-uvs = [[(v[0] + 1) / 2, (v[1] + 1) / 2] for v in vertices]
 
 custom_mesh = Mesh(
     vertices=vertices,
     triangles=faces,
     normals=normals,
-    uvs=uvs,
     mode='triangle'
 )
 
 dice = Entity(
-    model=custom_mesh, 
-    texture='brick',
-    color='#ff0000',
+    model=custom_mesh,
+    color='#6800ff',
+    normal_map='brick_normal',
     scale=2, 
     y=1, 
     shader=lit_with_shadows_shader
 )
 
 light = DirectionalLight(shadows=True)
-light.look_at(Vec3(0.5, -1, -1))
+light.look_at(Vec3(-0.5, -1, 1))
 
 selected_sides = 6 
 
 def set_sides(n):
     global selected_sides
+    global dice
     selected_sides = n
     sides_button.text = str(n)
 
+    dice_mesh = create_dice(n)
+    vertices = dice_mesh.vertices.tolist()
+    normals = dice_mesh.vertex_normals.tolist()
+    faces = [face[::-1] for face in dice_mesh.faces.tolist()]
+    
+    custom_mesh = Mesh(
+        vertices=vertices,
+        triangles=faces,
+        normals=normals,
+        mode='triangle'
+    )
+    if 'dice' in globals() and dice:
+            dice.disable()
+            destroy(dice)
+        
+    dice = Entity(
+        model=custom_mesh,
+        color='6800ff',
+        scale=2,
+        y=1,
+        shader=lit_with_shadows_shader
+    )
+
+def create_dice(n):
+    if n == 4:  # Tétraèdre
+        vertices = [
+            [0, 0, 1], 
+            [0.943, 0, -0.333], 
+            [-0.471, 0.816, -0.333], 
+            [-0.471, -0.816, -0.333]
+        ]
+        faces = [[0, 1, 2], [0, 2, 3], [0, 3, 1], [1, 3, 2]]
+        return trimesh.Trimesh(vertices=vertices, faces=faces)
+        
+    elif n == 6:  # Cube
+        return trimesh.creation.box(extents=[2, 2, 2])
+        
+    elif n == 8:  # Octaèdre
+        vertices = [
+            [1, 0, 0], [-1, 0, 0], 
+            [0, 1, 0], [0, -1, 0], 
+            [0, 0, 1], [0, 0, -1]
+        ]
+        faces = [
+            [0, 2, 4], [0, 4, 3], [0, 3, 5], [0, 5, 2],
+            [1, 4, 2], [1, 3, 4], [1, 5, 3], [1, 2, 5]
+        ]
+        return trimesh.Trimesh(vertices=vertices, faces=faces)
+    
+    elif n == 10:  # Pentagonal antiprisme
+        vertices = []
+        # Pentagone supérieur
+        for i in range(5):
+            angle = 2 * np.pi * i / 5
+            vertices.append([np.cos(angle), np.sin(angle), 0.5])
+        # Pentagone inférieur (tourné)
+        for i in range(5):
+            angle = 2 * np.pi * (i + 0.5) / 5
+            vertices.append([np.cos(angle), np.sin(angle), -0.5])
+        
+        faces = []
+        for i in range(5):
+            faces.append([i, (i+1)%5, i+5])
+        for i in range(5):
+            faces.append([(i+1)%5, (i+1)%5+5, i+5])
+
+        return trimesh.Trimesh(vertices=vertices, faces=faces)
+        
+    elif n == 12:  # Dodécaèdre
+        phi = (1 + np.sqrt(5)) / 2
+        vertices = [
+            [1, 1, 1], [1, 1, -1], [1, -1, 1], [1, -1, -1],
+            [-1, 1, 1], [-1, 1, -1], [-1, -1, 1], [-1, -1, -1],
+            [0, phi, 1/phi], [0, phi, -1/phi], [0, -phi, 1/phi], [0, -phi, -1/phi],
+            [1/phi, 0, phi], [1/phi, 0, -phi], [-1/phi, 0, phi], [-1/phi, 0, -phi],
+            [phi, 1/phi, 0], [phi, -1/phi, 0], [-phi, 1/phi, 0], [-phi, -1/phi, 0]
+        ]
+        
+        faces = [
+            [0, 8, 4, 14, 12], [0, 16, 17, 2, 12], [0, 8, 9, 1, 16],
+            [1, 9, 5, 15, 13], [1, 16, 17, 3, 13], [2, 10, 6, 14, 12],
+            [2, 10, 11, 3, 17], [3, 11, 7, 15, 13], [4, 8, 9, 5, 18],
+            [4, 18, 19, 6, 14], [5, 18, 19, 7, 15], [6, 10, 11, 7, 19]
+        ]
+        return trimesh.Trimesh(vertices=vertices, faces=faces)
+        
+    elif n == 20:  # Icosaèdre
+        return trimesh.creation.icosahedron()
+    
 radialmenu = RadialMenu(
     text='6',
     buttons=(
@@ -55,7 +143,7 @@ radialmenu = RadialMenu(
     enabled = False
 )
 
-Text(text='Number of sides:', x=-.575, y=-.15, scale=0.7)
+Text(text='Number of sides:', x=-.575, y=-.15, scale=0.7, color=color.black)
 sides_button = Button(
     text='6',
     x=-.5,
@@ -65,7 +153,7 @@ sides_button = Button(
 )
 
 roll_button = Button(text='Roll Dice', y=-.3, scale=(0.2, 0.05), color=color.azure)
-result_text = Text(text='Result: ', position=(-0.7, 0.4), scale=1.5)
+result_text = Text(text='Result: ', position=(-0.7, 0.4), scale=1.5, color=color.black)
 
 def roll_dice():
     
@@ -83,7 +171,7 @@ def roll_dice():
 def set_result(result):
     result_text.text = f'Result: {result}'
     result_text.color = color.green
-    invoke(lambda: setattr(result_text, 'color', color.white), delay=0.5)
+    invoke(lambda: setattr(result_text, 'color', color.black), delay=0.5)
 
 roll_button.on_click = roll_dice
 
